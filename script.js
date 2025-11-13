5b2a4e12a51880e90cf5d0dc2245c7122e618e53
Marcela dos Santos Capella <20231PF.INF_I0006@passofundo.ifsul.local>
2025-11-13 15:33:18 -0300

    Iniciando a conexão com o banco de dados

---
+++ script.js
@@ -0,0 +1,21 @@
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+
+const SUPABASE_URL = 'https://dsugctfsikpmfuyynjpg.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRzdWdjdGZzaWtwbWZ1eXluanBnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMwNDQwMDMsImV4cCI6MjA3ODYyMDAwM30.0V7P2C5mIq7JXN_iJtuy6ocgAtJ0zB3qtrOnBoo4kYoA';
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+async function testarConexao() {
+  try {
+    const { data, error } = await supabase.from('livros').select('*').limit(1);
+
+    if (error) throw error;
+
+    console.log('Conexão bem-sucedida!');
+    console.log('Exemplo de dado retornado:', data);
+  } catch (err) {
+    console.error('Erro ao conectar com o Supabase:', err.message);
+  }
+}
+
+testarConexao();
