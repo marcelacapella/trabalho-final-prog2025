62a69b495c41ce6a2bb2d9e61b400149b60c394b
Marcela <marcela.s.capella@gmail.com>
2025-12-11 11:53:38 -0300

    Deploy Vercel

--- api/index.js
+++
@@ -1,6 +0,0 @@
-// Conecta seu backend original ao Vercel como Serverless Function
-
-import serverless from "serverless-http";
-import app from "../backend/index.js";  // usa seu backend do jeito que está
-
-export const handler = serverless(app);
